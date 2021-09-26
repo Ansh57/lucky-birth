e7716f23a66ae5d4453fe757120ff2eef885e1d3
Ansh <46096778+Ansh57@users.noreply.github.com>
2021-09-27 00:11:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+var date=document.querySelector("#dob");
+var luckyNumber=document.querySelector("#lucky-number");
+var checkBtn=document.querySelector("#check-btn");
+var showOutput=document.querySelector("#poutput");
+var output=document.querySelector("#noutput");
+
+
+checkBtn.addEventListener('click',userInput);
+
+function userInput(){
+   
+    var dob=date.value;
+ sum=sumOfDob(dob);
+checkDateBirth(sum,luckyNumber.value);
+    
+}
+
+function sumOfDob(dob){
+    var sum=0;
+    dob=dob.replaceAll("-","");
+   
+    for(i=0;i<dob.length;i++){
+        sum=sum+Number(dob.charAt(i));   
+    }
+    return sum;
+}
+
+
+function checkDateBirth(sum,luckyNumber){
+
+if((sum%luckyNumber===0))
+{
+    showOutput.innerHTML="Wow, Your are Lucky Man";
+
+}
+else{
+    output.innerHTML="Oops! try once More!";
+}
+}
+
